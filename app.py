304da87ecef7651f673cdb24a172a507bd9b468a
bolekhan1508 <bolehanw@gmail.com>
2024-04-10 13:06:55 +0300

    create home tamplate

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 2,
+        'title': 'Software Engineer',
+        'company': 'Google',
+        'location': 'San Francisco',
+        'date': '2017-01-01',
+        'description': 'I am a software engineer at Google. I love to code.'
+    },
+    {
+        'id': 3,
+        'title': 'Developer',
+        'company': 'Microsoft',
+        'location': 'Lviv',
+        'date': '2024-01-01',
+        'description': 'I am a software engineer at Microsoft. I love coding.'
+    },
+    {
+        'id': 4,
+        'title': 'Software Engineer',
+        'company': 'Amazon',
+        'location': 'Lviv',
+        'date': '2024-01-01',
+        'description': 'I am a good boy.'
+    }
+]
+
 @app.route('/')
 def hello_world():
-    return 'Hello Jovian!'
+    return render_template('home.html', JOBS=JOBS)
 
 
 
